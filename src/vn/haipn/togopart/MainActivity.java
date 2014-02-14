b53047b216e68311b40b675675a7ed1ba3a20e6f
HaiPN <phamngochai.a7@gmail.com>
2014-02-14 09:31:45 +0700

    init project

---
+++ src/vn/haipn/togopart/MainActivity.java
@@ -0,0 +1,128 @@
+package vn.haipn.togopart;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class MainActivity extends ActionBarActivity  implements ActionBar.TabListener {
+
+	private ViewPager mViewPager;
+	private AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+	private ActionBar actionBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager(), this);
+		actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.setDisplayShowTitleEnabled(true);
+
+	    mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between different app sections, select the corresponding tab.
+                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
+                // Tab.
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter.
+            // Also specify this Activity object, which implements the TabListener interface, as the
+            // listener for when this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mAppSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+	    if (savedInstanceState != null) {
+	        actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+	    }  
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+	
+	/**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
+     * sections of the app.
+     */
+    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+    	Context mContext;
+        public AppSectionsPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            mContext = context;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+            
+                case 0:
+                    // The first section of the app is the most interesting -- it offers
+                    // a launchpad into the other demonstrations in this example application.
+                    return new MainFragment();
+                case 1:
+                	return new MarketPlaceFragment();
+                case 2:
+                	return new ShortlistedAds();
+                default:
+                    // The other sections of the app are dummy placeholders.
+                    
+                    return new Search();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+        	switch (position) {
+            case 0:
+                // The first section of the app is the most interesting -- it offers
+                // a launchpad into the other demonstrations in this example application.
+                return mContext.getString(R.string.tab_main);
+            case 1:
+            	return mContext.getString(R.string.tab_marketplace);
+            case 2:
+            	return mContext.getString(R.string.tab_shortlist_ads);
+            default:
+                // The other sections of the app are dummy placeholders.
+                return mContext.getString(R.string.tab_search);
+        }
+        }
+    }
+}
---
+++ src/vn/haipn/togopart/ShortlistedAds.java
@@ -0,0 +1,7 @@
+package vn.haipn.togopart;
+
+import android.support.v4.app.Fragment;
+
+public class ShortlistedAds extends Fragment{
+
+}
